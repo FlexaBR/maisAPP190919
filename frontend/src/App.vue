564d0b8e6efc199d39d81d7a10680ab96d416855
FlexaBR <r.gutheil@terra.com.br>
2019-10-21 10:41:39 -0300

    base backend e ini frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,50 @@
 <template>
 	<div id="app">
-		<h1>Versão Inicial</h1>
+		<Header title="+APP" :hideToggle="false"/>
+		<Menu />
+		<Content />
+		<Footer />
 	</div>
 </template>
 
 <script>
+import Header from "@/components/template/Header"
+import Menu from "@/components/template/Menu"
+import Content from "@/components/template/Content"
+import Footer from "@/components/template/Footer"
+
 export default {
 	name: "App",
+	components: { Header, Menu, Content, Footer }
 }
+// style
+// font-family referenciada no index.html
+// margin: 0 para app não ter margem
+// webkit e moz para deixar as fontes mais suaves
+// height 100vh... ocupa a tela inteira do APP
+// grid-template-rows: 60px 1fr 40px; ===> Header 60 1 frame para Conteudo (maximo q der) 3 40 para Footer 
+// grid-template-columns: 300px 1fr ===> 300 p Menu e o qe sobra para conteudo
+
 </script>
 
 <style>
+	* {
+		font-family: "Lato", sans-serif;
+	}
+	body {
+		margin: 0;
+	}
+	#app {
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+
+		height: 100vh;
+		display: grid;
+		grid-template-rows: 60px 1fr 40px;
+		grid-template-columns: 300px 1fr;
+		grid-template-areas:
+			"header header"
+			"menu content"
+			"menu footer";
+	}
 </style>
---
+++ frontend/src/components/template/Content.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="content"></div>
+</template>
+
+<script>
+export default {
+    name: 'Content'
+}
+
+// no style: classe content / na área do content um espaçamento (padding) de 20 para não
+// colar no header, footer ou parte lateral do conteudo.
+</script>
+
+<style>
+    .content {
+        grid-area: content;
+        background-color: #efefef;
+        padding: 20px;
+    }
+</style>
